bf39580224b71a93f059794ad2b1c1bf585b79cb
Cynthia (Qingwei) Li <Qingwei.Li@ec.gc.ca>
2015-10-06 10:17:11 -0400

    feat(core): buggy new code

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js','src/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint','uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# geoAPI
+
+Javascript library for web mapping.
